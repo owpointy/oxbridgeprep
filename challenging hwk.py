9227cd7735218730e24683f9c461bca5f41a0196
owpointy <alexmiller3146@hotmail.com>
2016-07-06 01:56:02 +0100

    Add files via upload
    dang this was hard

---
+++ challenging hwk.py
@@ -0,0 +1,26 @@
+rtn =""
+##challenging hwk
+nums = [1,2,3,4,5,6]
+outputs=[]
+for i in range (0,6):
+    if nums[i] % 2 != 0:
+        rtn=nums[i]
+        outputs.append(rtn)
+
+print(outputs[0])
+print(outputs[0])
+
+print(outputs[2])
+print(outputs[2])
+
+print(outputs[1])
+print(outputs[1])
+
+print(outputs[1])
+print(outputs[0])
+
+print(outputs[1])
+print(outputs[2])
+
+print(outputs[2])
+print(outputs[0])
